88cc8580aeb9a88232e8fceb63802f7d96ccb222
Martin449 <76888581+martino449@users.noreply.github.com>
2024-08-23 19:26:03 +0200

    Update vector.py

--- vector.py
+++ vector.py
@@ -260,3 +260,19 @@ if __name__ == "__main__":
 
         else:
             print("Comando non riconosciuto. Riprova.")
+
+
+
+
+#Fine del codice
+# ---------------------------------------------
+# Copyright (c) 2024 Mario Pisano
+#
+# Questo programma è distribuito sotto la licenza EUPL, Versione 1.2 o – non appena 
+# saranno approvate dalla Commissione Europea – versioni successive della EUPL 
+# (la "Licenza");
+# Puoi usare, modificare e/o ridistribuire il programma sotto i termini della 
+# Licenza. 
+# 
+# Puoi trovare una copia della Licenza all'indirizzo:
+# https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
